71cd4d0a33333a057c68ac6b5bdd65ad54be4b15
Eduardo Romero <earomero@gmail.com>
2018-05-06 23:41:27 -0500

    💄 - Adding `elements` to `abstract` and `comments`, so we can use the proper HTML Serialization on client-side. Adding missing meta for `link`.

--- prismic/PrismicModels.js
+++ prismic/PrismicModels.js
@@ -3,21 +3,31 @@ const jsonata = require('jsonata')
 const bookMappings = `{
   "Book": {
     	"id": $join(["prismic", "-", id]),
-        "abstract": $join(data.description.text),
-        "cover": data.cover.url,
-        "asin": data.asin,
-        "colorize": data.colorize,
-        "link": data.link.url,
-        "comments": $join(data.comments.text),
-        "rating": data.rating,
-        "title": data.title.text,
-        "tags": $filter(data.tags.tag, function($value) { $value }),
-        "slug": slugs[0]
+      "abstract": { 
+        "text": $join(data.description.text), 
+        "elements": data.description 
+      },
+      "cover": data.cover.url,
+      "asin": data.asin,
+      "colorize": data.colorize,
+      "link": {
+        "type": $lowercase(data.link.link_type),
+        "url": data.link.url,
+        "target": $lowercase(data.link.target)
+      },
+      "comments": { 
+        "text": $join(data.comments.text), 
+        "elements": data.comments 
+      },
+      "rating": data.rating,
+      "title": data.title.text,
+      "tags": $filter(data.tags.tag, function($value) { $value }),
+      "slug": slugs[0]
     },
     "meta": {
     	"href": href,
-        "slugs": slugs,
-        "lang": lang,
+      "slugs": slugs,
+      "lang": lang,
     	"updated": last_publication_date
     },
     "type": "books",
