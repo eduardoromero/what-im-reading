5d63f8ab0ccb73b323a6567bc76549ffcdd017f9
Eduardo Romero <earomero@gmail.com>
2018-05-06 23:39:09 -0500

    🔒 - Using `PRISMIC_API_TOKEN` from environment. Access token will be revoked after deployment, will use as fallback for dev purposes.

--- prismic.js
+++ prismic.js
@@ -4,16 +4,15 @@ const {Book} = require('./prismic/PrismicModels')
 
 const prismicEndpoint = 'https://must-read.prismic.io/api/v2' // TODO: API URL comes on the webhook too.
 const MS_PER_MINUTE = 60000
-const API_TOKEN = 'MC5XdTVQSVNVQUFDVUFSaGh4.LWIrKwwR77-977-9TSU-77-977-977-977-9QO-_vRzvv70kPO-_vSXvv73vv73vv70j77-977-9Mznvv70'
+const API_TOKEN = process.env.PRISMIC_API_TOKEN || 'MC5XdTVQSVNVQUFDVUFSaGh4.LWIrKwwR77-977-9TSU-77-977-977-977-9QO-_vRzvv70kPO-_vSXvv73vv73vv70j77-977-9Mznvv70'
 
 module.exports.webhook = (event, context) => {
   const data = JSON.parse(event.body)
 
   return Prismic
-    .api(prismicEndpoint, { accessToken: API_TOKEN })
+    .api(prismicEndpoint, {accessToken: API_TOKEN})
     .then((api) => {
       const TEN_MINUTES_AGO = new Date(Date.now() - (MS_PER_MINUTE * 10))
-      console.log(TEN_MINUTES_AGO)
 
       return api.query(
         Prismic.Predicates.dateAfter('document.last_publication_date', TEN_MINUTES_AGO),
@@ -29,7 +28,6 @@ module.exports.webhook = (event, context) => {
         return new Book(item)
       })
 
-
       return new Promise((resolve) => {
         const response = {
           statusCode: 200,
@@ -40,7 +38,6 @@ module.exports.webhook = (event, context) => {
           })
         }
 
-        console.log(response)
         resolve(response)
       })
     })
@@ -54,7 +51,6 @@ module.exports.webhook = (event, context) => {
           })
         }
 
-        console.log(response)
         resolve(response)
       })
     })
